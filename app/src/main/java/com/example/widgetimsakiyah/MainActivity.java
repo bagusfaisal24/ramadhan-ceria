371e0043a22a1e5765ee992047385159053e7c74
bagus faisal kh <bagus.faizal@dssconsulting.id>
2020-04-30 10:55:16 +0700

    get address

--- app/src/main/java/com/example/widgetimsakiyah/MainActivity.java
+++ app/src/main/java/com/example/widgetimsakiyah/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.widgetimsakiyah;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.hellow);
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            setAddress(location.getLatitude(), location.getLongitude());
+                        } else {
+                            Toast.makeText(getApplicationContext(), "GPS Tidak Aktif", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void setAddress(Double latitude, Double longitude) {
+        Geocoder geocoder;
+        List<Address> addresses = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses != null) {
+            if (addresses.size() > 0) {
+                Log.d("max", " " + addresses.get(0).getMaxAddressLineIndex());
+
+                String address = addresses.get(0).getAddressLine(0);
+                String city = addresses.get(0).getLocality();
+                String state = addresses.get(0).getAdminArea();
+                String country = addresses.get(0).getCountryName();
+                String postalCode = addresses.get(0).getPostalCode();
+                String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+
+                addresses.get(0).getAdminArea();
+                textView.setText(address);
+            }
+        }
+
     }
 }
